d5bfead6114b48c4165ed703becb30d5564ee0c5
Richelin Metellus <richelin.metellus@outlook.com>
2024-03-31 16:29:22 -0500

    autogenerate version file with cmake configure_file() command

--- 06_CMakeAutoGenFile/app/main.cc
+++ 06_CMakeAutoGenFile/app/main.cc
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <mylib.h>
+#include <config_version.hpp>
 
 int main()
 {
+    std::cout << "Project name: " << project_name << std::endl;
+    std::cout << "Project version: " << project_version << '\n';
     print_hello_world();
 
     return 0;
