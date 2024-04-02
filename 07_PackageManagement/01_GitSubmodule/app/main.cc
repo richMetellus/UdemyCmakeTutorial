b4cc57248570fccd6f726fe125019a802684a5a3
Richelin Metellus <richelin.metellus@outlook.com>
2024-04-01 19:28:43 -0500

    show how to add external git submodule and link it into the project

--- 07_PackageManagement/01_GitSubmodule/app/main.cc
+++ 07_PackageManagement/01_GitSubmodule/app/main.cc
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <mylib.h>
 #include <config_version.hpp>
+#include <nlohmann/json.hpp>
 
 int main()
 {
     std::cout << "Project name: " << project_name << std::endl;
     std::cout << "Project version: " << project_version << '\n';
+    
+    std::cout << "NLOHMANN JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "." 
+              << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
     print_hello_world();
 
     return 0;
