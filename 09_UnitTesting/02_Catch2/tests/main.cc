284104e9c4dd5e1d240aa40b39dfd4dfe1fd6239
Richelin Metellus <richelin.metellus@outlook.com>
2024-04-04 19:00:43 -0500

    added edge case that will expose subtle bug in the factorial fuction logic

--- 09_UnitTesting/02_Catch2/tests/main.cc
+++ 09_UnitTesting/02_Catch2/tests/main.cc
@@ -4,6 +4,8 @@
 
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    
+    REQUIRE( Factorial(0) == 1 );
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
