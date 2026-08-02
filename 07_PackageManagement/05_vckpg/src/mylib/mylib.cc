fc8f4d584f428f858a4afb7bab9d7468981f7278
Richelin Metellus <richelin.metellus@outlook.com>
2026-08-02 17:43:33 -0500

    copy 04_conan as 05_vcpkg as starting point for this new tutorial

---
+++ 07_PackageManagement/05_vckpg/src/mylib/mylib.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdint>
+#include "mylib.h"
+
+#ifdef PRINTER_ACTIVE
+/**
+ * @brief Prints out Hello World! to the console
+ * 
+ */
+void print_hello_world()
+{
+    std::cout << "Hello World!\n";
+}
+#endif
+
+std::uint32_t factorial(std::uint32_t number)
+{
+  return number <= 1 ? 1 : factorial(number - 1) * number;
+}
