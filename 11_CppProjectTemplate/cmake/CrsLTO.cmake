cccc357c7db67c5c2dc78935b2eca07219cfec9f
Richelin Metellus <richelin.metellus@cummins.com>
2026-08-07 16:42:22 -0500

    cmake scripts updates and enable warnings as default

--- 11_CppProjectTemplate/cmake/CrsLTO.cmake
+++ 11_CppProjectTemplate/cmake/CrsLTO.cmake
@@ -3,24 +3,29 @@ Enable Interprocedural Optimization (IPO)/Link Time Optimization (LTO)
 https://cmake.org/cmake/help/latest/module/CheckIPOSupported.html
 require cmake 3.9 or above
 ]]
-function(crs_target_enable_lto CRS_TARGET CRS_ENABLE)
-    if(NOT ${CRS_ENABLE})
-        message(STATUS "Not Enabling LTO!")
-        return()
-    endif()
+function(crs_target_enable_lto)
+    set(oneValueArgs CRS_TARGET CRS_ENABLE)
+    cmake_parse_arguments(
+        CRS_LTO
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN})
 
     include(CheckIPOSupported) # include a pre-defined cmake module
     #[[
         In CheckIPOSupported.cmake there is a function check_ipo_supported,
-        cmake will detect which compiler we are using and set the crs_result
+        cmake will detect which compiler we are using and set the result in 
+        the variable crs_result
     ]]
     check_ipo_supported(RESULT crs_result OUTPUT crs_output)
 
     if(crs_result)
-        message(STATUS "IPO/LTO is supported for ${CRS_TARGET} target!")
+        message(STATUS "IPO/LTO is supported: ${CRS_LTO_CRS_TARGET}")
         # This predefined cmake property, INTERPROCEDURAL_OPTIMIZATION, is set if LTO is supported by the compiler
-        set_property(TARGET ${CRS_TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ${CRS_ENABLE})
+        set_property(TARGET ${CRS_LTO_CRS_TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION
+                                                   ${CRS_LTO_CRS_TARGET})
     else()
-        message(WARNING "IPO/LTO is not supported!")
+        message(WARNING "IPO/LTO is not supported: ${CRS_LTO_CRS_TARGET}")
     endif()
-endfunction(crs_target_enable_lto)
+endfunction()
--- 11_CppProjectTemplate/cmake/CrsWarnings.cmake
+++ 11_CppProjectTemplate/cmake/CrsWarnings.cmake
@@ -2,28 +2,77 @@
 Description: This cmake user-defined function allows the user to enable compiler warnings 
              as errors.
 ]]
-function(crs_target_set_warnings CRS_TARGET CRS_ENABLE CRS_ENABLE_AS_ERRORS)
-    # check if the user if the user does not want to enable any warnings
-    if (NOT ${CRS_ENABLED})
-        message(STATUS "Warnings are Disabled for: ${CRS_TARGET}")
+function(crs_target_set_warnings)
+    set(oneValueArgs TARGET ENABLE AS_ERRORS)
+    cmake_parse_arguments(
+        CRS_TARGET_SET_WARNINGS
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN})
+
+    if(NOT ${CRS_TARGET_SET_WARNINGS_ENABLE})
+        message(STATUS "Warnings Disabled for: ${CRS_TARGET_SET_WARNINGS_TARGET}")
         return()
     endif()
+    message(STATUS "Warnings Active for: ${CRS_TARGET_SET_WARNINGS_TARGET}")
+    message(STATUS "Warnings as Errors: ${CRS_TARGET_SET_WARNINGS_AS_ERRORS}")
 
-    # set a collection of useful warnings of popular compilers
-    # https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
     set(CRS_MSVC_WARNINGS
-        /W4
-        /permissive-)
+        # Baseline
+        /W4 # Baseline reasonable warnings
+        /permissive- # standards conformance mode for MSVC compiler
+        # C and C++ Warnings
+        /w14242 # conversion from 'type1' to 'type1', possible loss of data
+        /w14254 # 'operator': conversion from 't1:field_bits' to 't2:field_bits'
+        /w14287 # unsigned/negative constant mismatch
+        /w14296 # expression is always 'boolean_value'
+        /w14311 # pointer truncation from 'type1' to 'type2'
+        /w44062 # enumerator in a switch of enum 'enumeration' is not handled
+        /w44242 # conversion from 'type1' to 'type2', possible loss of data
+        /w14826 # Conversion from 'type1' to 'type_2' is sign-extended
+        /w14905 # wide string literal cast to 'LPSTR'
+        /w14906 # string literal cast to 'LPWSTR'
+        # C++ Only
+        /w14263 # function does not override any base class virtual function
+        /w14265 # class has virtual functions, but destructor is not virtual
+        /w14640 # Enable warning on thread un-safe static member initialization
+        /w14928 # more than one implicitly user-defined conversion
+        /we4289 # nonstandard extension used: 'variable'
+    )
 
     set(CRS_CLANG_WARNINGS
+        # Baseline
         -Wall
-        -Wextra
-        -Wpedantic)
-    # GCC and Clang use similar Warning options
-    set(CRS_GCC_WARNINGS
-        ${CRS_CLANG_WARNINGS})
+        -Wextra # reasonable and standard
+        -Wshadow # if a variable declaration shadows one from a parent context
+        -Wpedantic # warn if non-standard is used
+        # C and C++ Warnings
+        -Wunused # warn on anything being unused
+        -Wformat=2 # warn on security issues around functions that format output
+        -Wcast-align # warn for potential performance problem casts
+        -Wconversion # warn on type conversions that may lose data
+        -Wsign-conversion # warn on sign conversions
+        -Wnull-dereference # warn if a null dereference is detected
+        -Wdouble-promotion # warn if float is implicit promoted to double
+        # C++ Warnings
+        -Wnon-virtual-dtor # if a class with virtual func has a non-virtual dest
+        -Wold-style-cast # warn for c-style casts
+        -Woverloaded-virtual # if you overload (not override) a virtual function
+        -Weffc++ # violations from Scott Meyers’ Effective C++
+    )
 
-    if(${CRS_ENABLE_AS_ERRORS})
+    # GCC and Clang use similar Warning options
+    # see a collection of useful warnings of popular compilers
+    # https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
+    set(CRS_GCC_WARNINGS
+        ${CRS_CLANG_WARNINGS}
+        -Wduplicated-cond # warn if if / else chain has duplicated conditions
+        -Wduplicated-branches # warn if if / else branches have duplicated code
+        -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+    )
+
+    if(${CRS_TARGET_SET_WARNINGS_AS_ERRORS})
         set(CRS_MSVC_WARNINGS ${CRS_MSVC_WARNINGS} /WX)
         set(CRS_CLANG_WARNINGS ${CRS_CLANG_WARNINGS} -Werror)
         set(CRS_GCC_WARNINGS ${CRS_GCC_WARNINGS} -Werror)
@@ -36,10 +85,11 @@ function(crs_target_set_warnings CRS_TARGET CRS_ENABLE CRS_ENABLE_AS_ERRORS)
         set(CRS_WARNINGS ${CRS_CLANG_WARNINGS})
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
         set(CRS_WARNINGS ${CRS_GCC_WARNINGS})
-        message(STATUS "GNU detected ${CRS_WARNINGS}")
     endif()
 
-    target_compile_options(${CRS_TARGET} PRIVATE ${CRS_WARNINGS})
+    message(STATUS "Compiler Warnings flags: ${CRS_WARNINGS}")
+
+    target_compile_options(${CRS_TARGET_SET_WARNINGS_TARGET} PRIVATE ${CRS_WARNINGS})
     message(STATUS "Compile Options: ${CRS_WARNINGS}")
 
 endfunction(crs_target_set_warnings)
