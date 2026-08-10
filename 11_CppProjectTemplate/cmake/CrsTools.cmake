0018cbe6bbea1638b5dc82e1ed362ffbdc68df72
Richelin Metellus <richelin.metellus@cummins.com>
2026-08-10 18:28:09 -0500

    add script that will enable cmake files formatting

--- 11_CppProjectTemplate/cmake/CrsTools.cmake
+++ 11_CppProjectTemplate/cmake/CrsTools.cmake
@@ -1,3 +1,41 @@
+function(crs_add_cmake_format_target)
+    if(NOT ${CRS_ENABLE_CMAKE_FORMAT})
+        return()
+    endif()
+    set(CRS_ROOT_CMAKE_FILES "${CMAKE_SOURCE_DIR}/CMakeLists.txt")
+    file(GLOB_RECURSE CRS_CMAKE_FILES_TXT "*/CMakeLists.txt")
+    file(GLOB_RECURSE CRS_CMAKE_FILES_C "cmake/*.cmake")
+    list(
+        FILTER
+        CRS_CMAKE_FILES_TXT
+        EXCLUDE
+        REGEX
+        "${CMAKE_SOURCE_DIR}/(build|external)/.*")
+    set(CRS_CMAKE_FILES ${CRS_ROOT_CMAKE_FILES} ${CRS_CMAKE_FILES_TXT} ${CRS_CMAKE_FILES_C})
+    find_program(CRS_CMAKE_FORMAT cmake-format)
+    if(CRS_CMAKE_FORMAT)
+        message(STATUS "Added Cmake Format")
+        set(CRS_FORMATTING_COMMANDS)
+        foreach(crs_cmake_file ${CRS_CMAKE_FILES})
+            list(
+                APPEND
+                CRS_FORMATTING_COMMANDS
+                COMMAND
+                cmake-format
+                -c
+                ${CMAKE_SOURCE_DIR}/.cmake-format.yaml
+                -i
+                ${crs_cmake_file})
+        endforeach()
+        add_custom_target(
+            crs_run_cmake_format
+            COMMAND ${CRS_FORMATTING_COMMANDS}
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    else()
+        message(WARNING "CRS_CMAKE_FORMAT NOT FOUND")
+    endif()
+endfunction()
+
 function(crs_add_clang_tidy_to_target crs_target)
     get_target_property(CRS_TARGET_SOURCES ${crs_target} SOURCES)
     list(
